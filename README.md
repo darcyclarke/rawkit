411f903a596c6cd8dfee9f0d890690be811e5967
Darcy Clarke <darcy@darcyclarke.me>
2017-05-20 01:18:04 -0400

    Update and add server

--- README.md
+++ README.md
@@ -18,3 +18,30 @@ $ rawkit index.js
 
 ### Should this be in node core?
 Probably. A flag like `--launch` would be a nice suppliment to `--inspect`. Maybe I'll make a PR one day.
+
+### Interesting command line flags for chrome
+
+- `--app`
+- `--auto-open-devtools-for-tabs`
+- `--allow-file-access-from-files`
+- `--enable-devtools-experiments`
+- `--enable-ui-devtools`
+- `--remote-debugging-targets`
+- `--isolate-extensions`
+- `--disable-web-security`
+- `--allow-file-access`
+- `--app-shell-host-window-size=w,h`
+- `--window-size=w,h`
+- `--window-position=x,y`
+
+#### Launch Chrome
+
+```bash
+$ /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ...
+```
+
+#### You can also pass in straight HTML to `--app`
+
+```bash
+$ ... --app="data:text/html,<html><body><script>window.moveTo(0,0);window.resizeTo(800,600);</script></body></html>""
+```
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 {
+  const fs = require('fs')
   const opn = require('opn')
+  const argv = require('yargs').argv
   const exec = require('child_process').exec
+  const http = require('http')
+
+  const server = http.createServer()
+  const port = argv.port || 1337
 
   let args = process.argv.splice(2, process.argv.length).join(' ')
   let cmd = `node --inspect ${args}`
   let child = exec(cmd, { shell: true })
   let search = true
+
+  server.on('request', (req, res) => {
+    fs.readFile('./extension/index.html', 'utf8', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data)
+    })
+  }).listen(port)
 
   function parse (str) {
     let re = /\schrome-devtools[^\s]*|^chrome-devtools[^\s]*/gi
@@ -16,24 +30,8 @@
   function handle (data) {
     let link = parse(data)
     if (search && link) {
-      console.log('FOUND!', link)
-
-      // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "data:text/html,<html><body><script>window.moveTo(0,0);window.resizeTo(800,600);</script></body></html>" --allow-file-access-from-files --disable-web-security --allow-file-access --new-window
-      // --app="data:text/html,<html><body><script>window.moveTo(0,0);window.resizeTo(800,600);window.location='http://darcyclarke.me';</script></body></html>"
-      // --auto-open-devtools-for-tabs
-      // --allow-file-access-from-files
-      // --enable-devtools-experiments
-      // --enable-ui-devtools
-      // --remote-debugging-targets
-      // --isolate-extensions
-      // --disable-web-security
-      // --allow-file-access
-      // --app-shell-host-window-size=WxH
-      // --window-size=W,H
-      // --window-position=x,y
-
-      console.log(`LINKS: https://google.com/?rawkit=${encodeURIComponent(link)}`)
-      opn(`https://google.com/?rawkit=${encodeURIComponent(link)}`, { app: ['google chrome canary'], wait: false }).then(() => {
+      console.log(`http://localhost:${port}/?rawkit=${encodeURIComponent(link)}`)
+      opn(`http://localhost:${port}/?rawkit=${encodeURIComponent(link)}`, { app: ['google chrome canary'], wait: false }).then(() => {
         console.log('opened!')
       })
       search = false
@@ -42,14 +40,8 @@
   }
 
   child.stdout.on('data', handle)
-
   child.stderr.on('data', handle)
 
-  child.on('close', () => {
-    process.exit()
-  })
-
-  process.on('exit', () => {
-    child.kill()
-  })
+  child.on('close', _ => process.exit())
+  process.on('exit', _ => child.kill())
 }
