33ed7851b70792be57ba968504533f9864661cb4
Darcy Clarke <darcy@darcyclarke.me>
2017-05-20 05:13:16 -0400

    Fix up index.js

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-{
-  const fs = require('fs')
-  const opn = require('opn')
-  const argv = require('yargs').argv
-  const exec = require('child_process').exec
-  const http = require('http')
+const fs = require('fs')
+const opn = require('opn')
+const argv = require('yargs').argv
+const exec = require('child_process').exec
+const http = require('http')
 
-  const server = http.createServer()
-  const port = argv.port || 1337
+const server = http.createServer()
+const port = argv.port || 1337
 
-  let args = process.argv.splice(2, process.argv.length).join(' ')
-  let child = exec(`node --inspect ${args}`, { shell: true })
-  let search = true
+let args = process.argv.splice(2, process.argv.length).join(' ')
+let child = exec(`node --inspect ${args}`, { shell: true })
+let search = true
 
-  server.on('request', (req, res) => {
-    fs.readFile('./extension/index.html', 'utf8', (err, data) => {
-      if (err) throw err
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.end(data)
+server.on('request', (req, res) => {
+  fs.readFile('./extension/index.html', 'utf8', (err, data) => {
+    if (err) throw err
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(data)
+  })
+}).listen(port)
+
+function parse (str) {
+  let re = /\schrome-devtools[^\s]*|^chrome-devtools[^\s]*/gi
+  let matches = str.match(re)
+  return (matches) ? matches[0] : null
+}
+
+function handle (data) {
+  let link = parse(data)
+  if (search && link) {
+    opn(`http://localhost:${port}/?rawkit=${encodeURIComponent(link)}`, { app: ['google chrome canary'], wait: false }).then(() => {
+      console.log('rawkit...')
     })
-  }).listen(port)
-
-  function parse (str) {
-    let re = /\schrome-devtools[^\s]*|^chrome-devtools[^\s]*/gi
-    let matches = str.match(re)
-    return (matches) ? matches[0] : null
+    search = false
   }
+  process.stdout.write(data)
+}
 
-  function handle (data) {
-    let link = parse(data)
-    if (search && link) {
-      opn(`http://localhost:${port}/?rawkit=${encodeURIComponent(link)}`, { app: ['google chrome canary'], wait: false }).then(() => {
-        console.log('rawkit...')
-      })
-      search = false
-    }
-    process.stdout.write(data)
-  }
+child.stdout.on('data', handle)
+child.stderr.on('data', handle)
 
-  child.stdout.on('data', handle)
-  child.stderr.on('data', handle)
+child.on('close', _ => process.exit())
+process.on('exit', _ => child.kill())
 
-  child.on('close', _ => process.exit())
-  process.on('exit', _ => child.kill())
+module.export = {
+  handle: handle
 }
