5054ed813392a23cac3d07afc72499e4df5f2cf5
Darcy Clarke <darcy@darcyclarke.me>
2017-08-19 17:42:10 -0400

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -7,11 +7,18 @@ const rawkit = require('../bin/cli')(args)
 describe('rawkit', () => {
   // TODO:
   // - should parse arguments
-  // - should start a node child process
-  // - should start extension server
+  // - should handle custom executable
+  // - should handle canary
+  // - should handle nodemon
+  // - should handle reload/restart
+  // - should handle inspect-brk
+  // - should handle inspect-port
+  // - should handle stdin stdin(input)
+
   // - should detect url
   // - should parse url
-  // - should handle stdin stdin(input)
+
+  // - should start a child process
 
   it('should handle arguments', () => {
     expect(rawkit.args.test).to.equal('args')
